24a5430300cb5cdb83d9affbcef0536910056e09
younjunsu <junsu_youn@naver.com>
2022-11-29 12:45:16 +0900

    update

---
+++ run.sh
@@ -0,0 +1,16 @@
+##########################################
+# CUBRID installation script
+##########################################
+# user configuration
+export binary=
+export db_name=testdb
+export db_charset=ko_KR.utf8
+export ha_mode=on #on or off
+
+
+##########################################
+
+
+
+
+##########################################
